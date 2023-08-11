eb0583785809676a734cad830645929cd3f7efe1
Rajvi Zala <rajvisinhazala@gmail.com>
2023-08-11 23:30:55 +0530

    Commiting changes before pulling

---
+++ server/api/urls.py
@@ -0,0 +1,7 @@
+from . views import *
+from django.urls import path
+
+urlpatterns = [
+    path('', index),
+    path('getBooks/',getBook)
+]
--- server/server/urls.py
+++ server/server/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('library/', include('library.urls')),
     path('notes/', include('notes.urls')),
     path('ai/', include('ai.urls')),
+    path('api/', include('api.urls')),
 ]
