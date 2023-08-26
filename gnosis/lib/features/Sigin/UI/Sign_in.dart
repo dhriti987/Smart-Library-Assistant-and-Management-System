160682f3ea5a99b62c59b1489bc4a96201096f83
Pal202 <palpatel16112@gmail.com>
2023-08-26 21:36:39 +0530

    signin page with route

--- gnosis/lib/features/Sigin/UI/Sign_in.dart
+++ gnosis/lib/features/Sigin/UI/Sign_in.dart
@@ -1,123 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SignIn extends StatelessWidget {
   const SignIn({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Center(
-          child: Text(
-            'Explore the World of',
-            style: TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
+    TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
+      body: Container(
+        height: double.maxFinite,
+        width: double.maxFinite,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/video/library_bg_gif.gif'),
+            fit: BoxFit.cover,
           ),
         ),
-        Center(
-          child: Text(
-            'Books',
-            style: TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 35,
-        ),
-        Container(
-          margin: EdgeInsets.only(left: 40),
-          child: Text(
-            'Email',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        SizedBox(
-          height: 15,
-        ),
-        Container(
-          margin: EdgeInsets.only(left: 20, right: 20),
-          child: TextField(
-            decoration: InputDecoration(
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-              filled: true,
-              fillColor: Colors.white,
-              hintText: 'Enter your email address',
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        Container(
-          margin: EdgeInsets.only(left: 40),
-          child: Text(
-            'Password',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        SizedBox(
-          height: 15,
-        ),
-        Container(
-          margin: EdgeInsets.only(left: 20, right: 20),
-          child: TextField(
-            decoration: InputDecoration(
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-              filled: true,
-              fillColor: Colors.white,
-              hintText: 'Enter your password',
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          alignment: Alignment.centerRight,
-          margin: EdgeInsets.only(right: 30),
-          child: Text(
-            'Forget your password?',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 16,
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 25,
-        ),
-        Center(
-          child: ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              primary: Color.fromARGB(250,205, 97, 129),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              )
-            ),
-            child: Container(
-              padding: EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
               child: Text(
-                'Login',
+                'Explore the World of \nBooks',
+                style: textTheme.headlineLarge,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 40),
+              child: Text(
+                'Email',
+                style: textTheme.labelMedium,
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20, right: 20),
+              child: TextField(
+                decoration: InputDecoration()
+                    .applyDefaults(Theme.of(context).inputDecorationTheme)
+                    .copyWith(hintText: 'Enter email address'),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 40),
+              child: Text(
+                'Password',
+                style: textTheme.labelMedium,
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20, right: 20),
+              child: TextField(
+                decoration: InputDecoration()
+                    .applyDefaults(Theme.of(context).inputDecorationTheme)
+                    .copyWith(hintText: 'Enter password'),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              alignment: Alignment.centerRight,
+              margin: EdgeInsets.only(right: 30),
+              child: Text(
+                'Forget your password?',
                 style: TextStyle(
                   color: Colors.white,
-                  fontSize: 20,
+                  fontSize: 16,
                 ),
               ),
             ),
-          ),
-        )
-      ],
+            SizedBox(
+              height: 28,
+            ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  context.go('/');
+                },
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- gnosis/lib/main.dart
+++ gnosis/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:gnosis/features/Sigin/UI/Sign_in.dart';
-import 'package:video_player/video_player.dart';
+import 'package:gnosis/core/theme/common_theme.dart';
+import 'core/router/router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,62 +11,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp.router(
+      // routeInformationParser: AppRouter().router.routeInformationParser,
+      // routerDelegate: AppRouter().router.routerDelegate,
+      routerConfig: AppRouter().getRouter(true),
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late VideoPlayerController _playerController;
-  @override
-  void initState() {
-    _playerController =
-        VideoPlayerController.asset('assets/video/library_gif.mp4')
-          ..initialize().then((_) {
-            _playerController.play();
-            _playerController.setLooping(true);
-            setState(
-              () {},
-            );
-          });
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          SizedBox.expand(
-            child: FittedBox(
-              fit: BoxFit.cover,
-              child: SizedBox(
-                width: _playerController.value.size.width,
-                height: _playerController.value.size.height,
-                child: VideoPlayer(_playerController),
-              ),
-            ),
-          ),
-          // Center(child: Text('Hello')),
-          SignIn(),
-        ],
-      ),
+      theme: darkTheme,
     );
   }
 }
