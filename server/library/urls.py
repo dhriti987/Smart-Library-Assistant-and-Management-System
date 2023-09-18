9b526a745fb84cae2d756cb2e3330cc2a6d64ebf
Rajvi Zala <rajvisinhazala@gmail.com>
2023-09-18 19:08:54 +0530

    Author List API

--- server/library/urls.py
+++ server/library/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 urlpatterns = [
     path('', getBook),
     path('getBooks/',getBook),
+    path('getAuthors/',AllAuthorList.as_view()),
+    path('searchBooks/',SearchBooks.as_view()),
     path('best-books/',TopRatedBookAPIView.as_view()),
     path('recent-books/',MostRecentBooks.as_view()),
     path('liked-books/<user_id>',MostLikedBooks.as_view()),
@@ -11,5 +13,5 @@ urlpatterns = [
     path('all-categories/',AllGenreBooks.as_view()),
     path('recommendations/', RecommendationAPIView.as_view(), name='recommendation'),
     path('genre/<str:category>/', GenreBooksAPIView.as_view()),
-
+    path('author-books/<str:author>/',AuthorBook.as_view()),
 ]
